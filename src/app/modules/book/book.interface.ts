fc92741e495e81d97b4e48343671661e4582af95
abir14016 <abir14016@gmail.com>
2023-08-03 16:04:44 +0600

    [DONE] create user interface

--- src/app/modules/book/book.interface.ts
+++ src/app/modules/book/book.interface.ts
@@ -1,10 +1,12 @@
-import { Model } from "mongoose";
+import { Model, Types } from "mongoose";
 
 export type IBook = {
   title: string;
   author: string;
   genre: string;
+  imageURL?: string;
   publicationYaer: number;
+  owner: Types.ObjectId;
 };
 
 export type BookModel = Model<IBook, Record<string, unknown>>;
