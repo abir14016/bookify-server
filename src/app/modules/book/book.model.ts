583f937e461865ba38a8b77e78fbacfb53ac6fda
abir14016 <abir14016@gmail.com>
2023-08-03 17:24:12 +0600

    [DONE] create book model

---
+++ src/app/modules/book/book.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import { BookModel, IBook } from "./book.interface";
+
+const BookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+    },
+    publicationYaer: {
+      type: Number,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+//model
+export const Book = model<IBook, BookModel>("Book", BookSchema);
