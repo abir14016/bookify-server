61b5163a2a3a4882efaf1cc85cecea85ac2b16c2
abir14016 <abir14016@gmail.com>
2023-08-03 18:07:19 +0600

    [DONE] prevent uploading a book with the _id which is not exist in database

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,8 +1,15 @@
+import httpStatus from "http-status";
+import ApiError from "../../../errors/ApiError";
+import { User } from "../user/user.model";
 import { IBook } from "./book.interface";
 import { Book } from "./book.model";
 
 //create book function
 const createBook = async (payload: IBook): Promise<IBook> => {
+  const isOwnerExist = await User.findById(payload.owner);
+  if (!isOwnerExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
+  }
   const result = (await Book.create(payload)).populate("owner");
   return result;
 };
