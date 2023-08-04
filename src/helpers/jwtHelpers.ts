5d5238c68e8f2c554caeff764eda154157c6f138
abir14016 <abir14016@gmail.com>
2023-08-04 10:30:02 +0600

    [DONE] utility function for create and verify token

---
+++ src/helpers/jwtHelpers.ts
@@ -0,0 +1,22 @@
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+
+//utility function for creating token
+const createToken = (
+  payload: Record<string, unknown>,
+  secret: Secret,
+  expireTime: string,
+): string => {
+  return jwt.sign(payload, secret, {
+    expiresIn: expireTime,
+  });
+};
+
+//utility function for verifying token
+const verifyToken = (token: string, secret: Secret): JwtPayload => {
+  return jwt.verify(token, secret) as JwtPayload;
+};
+
+export const JwtHelpers = {
+  createToken,
+  verifyToken,
+};
