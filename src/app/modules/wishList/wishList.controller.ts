1e88902e8601615f3063c5b2791b12f810d9fdd7
abir14016 <abir14016@gmail.com>
2023-08-12 13:43:06 +0600

    [DONE] create wishlist controller

---
+++ src/app/modules/wishList/wishList.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import { IWishList } from "./wishList.interface";
+import { WishListService } from "./wishList.service";
+
+//controller for creating a book
+const addToWishList = catchAsync(async (req: Request, res: Response) => {
+  const { ...wishListData } = req.body;
+  const result = await WishListService.addToWishList(wishListData);
+
+  sendResponse<IWishList>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: `Successfully added to wishlist`,
+    data: result,
+  });
+});
+
+export const WishListController = {
+  addToWishList,
+};
